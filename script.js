677c82d48aa529c76b50dfe165cdd4a6787d95cc
alxqemiste <alexander.schreier@schreierei.de>
2023-11-03 17:30:08 +0100

    03.11.2023

--- script.js
+++ script.js
@@ -1,185 +1,263 @@
-//Navbar-Buttons
-let navBarStartBtn = document.getElementById("navBarStartBtn")
-let navBarAboutBtn = document.getElementById("navBarAboutBtn")
-let navBarKrisenBtn = document.getElementById("navBarKrisenBtn")
-let navBarSpendenBtn = document.getElementById("navBarSpendenBtn")
+        //Navbar-ButtonsnavBarAboutUsBtn
+        const navBarStartBtn = document.getElementById("navBarStartBtn")
+        const navBarAboutUsBtn = document.getElementById("navBarAboutUsBtn")
+        const navBarCrisesBtn = document.getElementById("navBarCrisesBtn")
+        const navBarDonationBtn = document.getElementById("navBarDonationBtn")
+        
+        //Cards
+        const cardStart = document.getElementById("cardStart")
+        const cardRegistrationForm = document.getElementById("cardRegistrationForm")
+        const cardError = document.getElementById("cardError")
+        const cardSuccess = document.getElementById("cardSuccess")
+        
+        //Card-Buttons
+        const btnHandover = document.getElementById("btnHandover")
+        const btnPickup = document.getElementById("btnPickup")
+        
+        //Inputfelder
+        const inputFirstName = document.getElementById("inputFirstName")
+        const inputLastName = document.getElementById("inputLastName")
+        const inputStreet = document.getElementById("inputStreet")
+        const inputZIPCode = document.getElementById("inputZIPCode")
+        const inputCity = document.getElementById("inputCity")
+        const selectClothes = document.getElementById("selectClothes")
+        const selectConflictArea= document.getElementById("selectConflictArea")
+        const addBtn = document.getElementById("addBtn")
+        const submitBtn = document.getElementById("submitBtn")
+        const btnNewDonation = document.getElementById("btnNewDonation")
+        
+        //Kleidungsliste
+        const tableOfClothes = document.getElementById("tableOfClothes")
+        
+        //Variablen
+        let kindOfTransaction = "Übergabe"
+        let listOfClothes = []
 
-//Cards
-let cardWillkommen = document.getElementById("cardWillkommen")
-let cardFormular = document.getElementById("cardFormular")
-let cardError = document.getElementById("cardError")
+        window.onload = function() {
+            showStart();
+        }
+        
+        navBarStartBtn.onclick = function () {
+            showStart();
+        }
+        
+        navBarAboutUsBtn.onclick = function () {
+            showError();
+        }
+        
+        navBarCrisesBtn.onclick = function () {
+            showError();
+        }
+        
+        navBarDonationBtn.onclick = function () {
+            showRegistrationForm();
+        }
+        
+        btnHandover.onclick = function () {
+            kindOfTransaction = "Übergabe"
+            btnHandover.classList.add("active")
+            btnPickup.classList.remove("active")
+        
+            inputFirstName.style.display = "none";
+            inputLastName.style.display = "none";
+            inputStreet.style.display = "none";
+            inputZIPCode.style.display = "none";
+            inputCity.style.display = "none";
+        }
+        
+        btnPickup.onclick = function () {
+            kindOfTransaction = "Abholung"
+            btnPickup.classList.add("active")
+            btnHandover.classList.remove("active")
+            inputFirstName.style.display = "block";
+            inputLastName.style.display = "block";
+            inputStreet.style.display = "block";
+            inputZIPCode.style.display = "block";
+            inputCity.style.display = "block";
+        }
+        
+        addBtn.onclick = function () {
+            if(selectClothes.value != "") {
 
-//Card-Buttons
-let btnUebergabe = document.getElementById("btnUebergabe")
-let btnAbholung = document.getElementById("btnAbholung")
+                let alreadyChosen = false
 
-//Inputfelder
-let inputVorname = document.getElementById("inputVorname")
-let inputNachname = document.getElementById("inputNachname")
-let inputStraße = document.getElementById("inputStraße")
-let inputPLZ = document.getElementById("inputPLZ")
-let inputOrt = document.getElementById("inputOrt")
-let selectKleidung= document.getElementById("selectKleidung")
-let selectKrisengebiet= document.getElementById("selectKrisengebiet")
-let addBtn = document.getElementById("addBtn")
-let submitBtn = document.getElementById("submitBtn")
+                for(i=0; i<listOfClothes.length; i++) {
+                    if (listOfClothes[i] === selectClothes.value) {
+                        console.log("Kleidungsstück wurde schon gewählt")
+                        alreadyChosen = true;
+                    }
+                }
 
-//Kleidungsliste
-let listeKleider = document.getElementById("listeKleider")
+                if (alreadyChosen === false) {
+                    listOfClothes.push(selectClothes.value) 
+                    tableOfClothes.innerHTML += "<li  class='list-group-item'>"+selectClothes.value+"</li>"
+                    selectClothes.style["border-color"] = "lightgrey"
+                }
 
-//Variablen
-let artDerUebergabe = "Übergabe"
-let listeKleidungsstücke = []
+                
+            } else {
+                selectClothes.style["border-color"] = "red"
+            }
 
-navBarStartBtn.onclick = function () {
-    //console.log("Start-Button geklickt")  //Test
+        }
+        
+        submitBtn.onclick = function () {
+            if(kindOfTransaction === "Übergabe") {
+                
+                if (checkIfArrayIsEmpty(listOfClothes) && checkIfInputIsFilled (selectConflictArea)) {
+                    
+                    inputZIPCode.value = "93049"
+                    buildSuccessCard();
 
-    cardWillkommen.style.display = "block"
-    cardFormular.style.display = "none"
-    cardError.style.display = "none"
+        
+                } else { 
+                    console.log("Fehler") //TODO Errorhandling
+                }
+        
+        
+            } else if (kindOfTransaction === "Abholung") {
 
-}
+                checkInputs();
+                
+                              
+        
+            } else {
+                console.log("Fehler") //TODO Errorhandling
+            }
+        
+        }
 
-navBarAboutBtn.onclick = function () {
-    //console.log("Über Uns Button geklickt")  //Test
+        btnNewDonation.onclick = function () {
+            inputFirstName.value = ""
+            inputLastName.value = ""
+            inputStreet.value = ""
+            inputZIPCode.value = ""
+            inputCity.style.value = ""
+            selectClothes.value = ""
+            selectConflictArea.value = ""
+            listOfClothes = []
 
-    cardWillkommen.style.display = "none"
-    cardFormular.style.display = "none"
-    cardError.style.display = "block"
-}
+            showRegistrationForm();
+        }
 
-navBarKrisenBtn.onclick = function () {
-    //console.log("Krisen-Button geklickt")  //Test
+        
+        function showStart () {
+            cardStart.style.display = "block"
+            cardRegistrationForm.style.display = "none"
+            cardError.style.display = "none"
+            cardSuccess.style.display = "none"
 
-    cardWillkommen.style.display = "none"
-    cardFormular.style.display = "none"
-    cardError.style.display = "block"
-}
+        }
 
-navBarSpendenBtn.onclick = function () {
-    //console.log("Spenden-Button geklickt")  //Test
+        function showError() {
+            cardStart.style.display = "none"
+            cardRegistrationForm.style.display = "none"
+            cardError.style.display = "block"
+            cardSuccess.style.display = "none"
 
-    cardWillkommen.style.display = "none"
-    cardFormular.style.display = "block"
-    cardError.style.display = "none"
-}
+        }
 
-btnUebergabe.onclick = function () {
-    //console.log("Übergabe!")
+        function showSuccess() {
+            cardSuccess.style.display = "block"
 
-    artDerUebergabe = "Übergabe"
-    btnUebergabe.classList.add("active")
-    btnAbholung.classList.remove("active")
+        }
 
-    inputVorname.style.display = "none";
-    inputNachname.style.display = "none";
-    inputStraße.style.display = "none";
-    inputPLZ.style.display = "none";
-    inputOrt.style.display = "none";
-}
+        function showRegistrationForm() {
+            cardStart.style.display = "none"
+            cardRegistrationForm.style.display = "block"
+            cardError.style.display = "none"
+            cardSuccess.style.display = "none"
+        }
 
-btnAbholung.onclick = function () {
-    //console.log("Abholung!")
+        function checkZIPCode() {
+            const digits = inputZIPCode.value.toString().slice(0,2)
+            if (digits === "93") {
+                buildSuccessCard();
+            } else {
+                alert("Es tut uns leid! Leider ist Deine Abholungsadresse zu weit weg von unserer Regensburger Filiale!")
+                //TODO Fehlerhandling
+            }
+      
+            
+            
+        }
 
-    artDerUebergabe = "Abholung"
-    btnAbholung.classList.add("active")
-    btnUebergabe.classList.remove("active")
-    inputVorname.style.display = "block";
-    inputNachname.style.display = "block";
-    inputStraße.style.display = "block";
-    inputPLZ.style.display = "block";
-    inputOrt.style.display = "block";
-}
+        function checkInputs () {
+            const inputs = [inputFirstName, inputLastName, inputStreet, inputZIPCode, inputCity, selectConflictArea]
+            let counter = 0
+    
+            for (i = 0; i < inputs.length; i++) {
+                if(checkIfInputIsFilled(inputs[i])) {
+                    counter += 1;
+                }
+            } 
 
-addBtn.onclick = function () {
-    if(selectKleidung.value != "") {
-        listeKleidungsstücke.push(selectKleidung.value) 
-        listeKleider.innerHTML += "<li  class='list-group-item'>"+selectKleidung.value+"</li>"
-    }
-    //console.log(selectKleidung.value)
-    //console.log(listeKleidungsstücke)
-}
-
-submitBtn.onclick = function () {
-    //console.log("SUBMIT")
+            if (inputZIPCode.value.length != 5) {
+                inputZIPCode.style["border-color"] = "red"
+                counter = 0
+             
+            } else {
+                inputZIPCode.style["border-color"] = "lightgrey"
+            }
 
     
-    if(artDerUebergabe === "Übergabe") {
-        
-        if (checkIfArrayIsEmpty(listeKleidungsstücke) && checkIfInputIsFilled (selectKrisengebiet)) {
-            console.log("Success, Übergabe an Geschäftsstelle erfolgreich!")
-
-            let successCardBody = document.getElementById("successCardBody")
-
-            let k = ""
-
-            k += "<p class='card-text'>Kleidung: " 
-
-            for(i=0; i<listeKleidungsstücke.length; i++) {
-               if(listeKleidungsstücke.length === 1) {
-                k += listeKleidungsstücke[i];
-               } else {
-                k += listeKleidungsstücke[i] + " ,";
-               }
-                
-                
+            if(counter === 6 && checkIfArrayIsEmpty(listOfClothes)) {
+                console.log("Success, Abholung erfolgreich gebucht!")
+                checkZIPCode();
+            } else {
+                console.log("Fehler") //TODO Errorhandling
             }
+        }
+        
+        function checkIfArrayIsEmpty (arr) {
             
-            k += "</p>"       
+            if (arr.length > 0) {
+                selectClothes.style["border-color"] = "lightgrey"
+                return true;
+            } else { 
+                selectClothes.style["border-color"] = "red"
+                return false; }
+        }
+        
+        function checkIfInputIsFilled (input) {
+            if (input.value != "") {
+                console.log("Erfolg in InputIsFilled");
+                input.style["border-color"] = "lightgrey";
+                return true; 
+            } else { 
+                console.log("Fehler in InputIsFilled");
+                input.style["border-color"] = "red";
+                return false; 
+            }
+        }
+
+        function buildSuccessCard () {
+
+            const date = new Date();
+            const timeStampDate = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear()
+            const timeStampTime = date.getHours() + ":"  + date.getMinutes()
+            
+            const successCardBody = document.getElementById("successCardBody")
+        
+            let k = ""
+            let clothes = ""
+
+            k += "<p class='card-text'>Kleidung: " + listOfClothes.join(", ")
+
+            k += "</p> <p class='card-text'>Krisengebiet: "  
+
+            k += selectConflictArea.value 
+
+            k += "</p> <p class='card-text'>Datum: " + timeStampDate
+            
+            k += "</p> <p class='card-text'>Uhrzeit: " + timeStampTime
+
+            k += "</p> <p class='card-text'>Ort: " + inputZIPCode.value + "</p>"
             
             successCardBody.innerHTML = k
-            
-            
-
-
-
-
-        } else { 
-            console.log("Fehler") //TODO Errorhandling
+            showSuccess();
         }
 
 
-    } else if (artDerUebergabe === "Abholung") {
         
-        let inputs = [inputVorname, inputNachname, inputStraße, inputPLZ, inputOrt, selectKrisengebiet]
-        let iterable = false
-
-        for (i = 0; i < inputs.length; i++) {
-            iterable = checkIfInputIsFilled(inputs[i]);
-        } 
-
-        if(iterable && checkIfArrayIsEmpty(listeKleidungsstücke)) {
-            console.log("Success, Abholung erfolgreich gebucht!")
-        } else {
-            console.log("Fehler") //TODO Errorhandling
-        }
-       
-
-    } else {
-        console.log("Fehler") //TODO Errorhandling
-    }
-
-}
-
-function checkIfArrayIsEmpty (arr) {
-    
-    if (arr.length > 0) {
-        selectKleidung.style["border-color"] = "green"
-        return true;
-    } else { 
-        selectKleidung.style["border-color"] = "red"
-        return false; }
-}
-
-function checkIfInputIsFilled (input) {
-    if (input.value != "") {
-        console.log("Erfolg in InputIsFilled");
-        input.style["border-color"] = "green";
-        return true; 
-    } else { 
-        console.log("Fehler in InputIsFilled");
-        input.style["border-color"] = "red";
-        return false; 
-    }
-}
-
+        
