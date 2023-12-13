3866ff8a5c57490c1ff5f19986b0719ecdc048b3
alxqemiste <alexander.schreier@schreierei.de>
2023-12-13 18:13:46 +0100

    13.12 fin

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-//Navbar-ButtonsnavBarAboutUsBtn
+//Referencing NavBar-Buttons as constants to use
 const navBarStartBtn = document.getElementById("navBarStartBtn")
 const navBarAboutUsBtn = document.getElementById("navBarAboutUsBtn")
 const navBarCrisesBtn = document.getElementById("navBarCrisesBtn")
 const navBarDonationBtn = document.getElementById("navBarDonationBtn")
 
-//Cards
+//Referencing cards to be able to show or hide them, when necessary
 const cardStart = document.getElementById("cardStart")
 const cardRegistrationForm = document.getElementById("cardRegistrationForm")
 const cardError = document.getElementById("cardError")
 const cardSuccess = document.getElementById("cardSuccess")
 const cardLoremIpsum = document.getElementById("cardLoremIpsum")
 
-    
-//Card-Buttons
+//Referencing forms, buttons and the list of clothes inside the registrationform 
 const btnHandover = document.getElementById("btnHandover")
 const btnPickup = document.getElementById("btnPickup")
-
-//Inputfelder
 const inputFirstName = document.getElementById("inputFirstName")
 const inputLastName = document.getElementById("inputLastName")
 const inputStreet = document.getElementById("inputStreet")
@@ -27,22 +24,26 @@ const selectConflictArea= document.getElementById("selectConflictArea")
 const addBtn = document.getElementById("addBtn")
 const submitBtn = document.getElementById("submitBtn")
 const btnNewDonation = document.getElementById("btnNewDonation")
-
-//Kleidungsliste
 const tableOfClothes = document.getElementById("tableOfClothes")
 
-//Variablen
+//Introducing variables used for functionality
 let kindOfTransaction = "Übergabe"
 let listOfClothes = []
 
+
+//function, that starts the webapplication with the start-page
 window.onload = function() {
     showStart();
 }
 
+/*
+Following up, there will be a few functions to implement the functionality of the navigationbar. 
+Different buttons will load and hide different cards
+*/
+
 navBarStartBtn.onclick = function () {
     showStart();
 }
-
         
 navBarAboutUsBtn.onclick = function () {
     showError();
@@ -56,6 +57,11 @@ navBarDonationBtn.onclick = function () {
     showRegistrationForm();
 }
 
+/*
+This onclick-eventhandler hides unneccessary forms inside the registrationcard, if the option "handover" is chosen.
+It also saves the kind of transaction for later use in the variable "kindOfTransaction"
+*/
+
 btnHandover.onclick = function () {
     kindOfTransaction = "Übergabe"
     btnHandover.classList.add("active")
@@ -68,6 +74,10 @@ btnHandover.onclick = function () {
     inputCity.style.display = "none";
 }
 
+/*
+Same with this eventhandler, but for the option "pickup"
+*/
+
 btnPickup.onclick = function () {
     kindOfTransaction = "Abholung"
     btnPickup.classList.add("active")
@@ -79,6 +89,14 @@ btnPickup.onclick = function () {
     inputCity.style.display = "block";
 }
 
+
+/*
+onclick-eventhandler to add a piece of clothes to the list of clothes.
+At first it checks, that the input isn't empty. If it is empty, the border of the inputform will be red.
+Then it introduces the variable "alreadyChosen" as false and iterates over the list of clothes to assure, that this peace of clothes hasnt't been picked already.
+This way, clothes cannot be chosen more than once.
+If it hasn't been picked, the function pushes the clothing to the array "listOfClothes" and also adds a listelement to the list in the registrationcard.
+*/
 addBtn.onclick = function () {
     if(selectClothes.value != "") {
 
@@ -104,6 +122,15 @@ addBtn.onclick = function () {
 
 }
 
+
+/*
+The submitbutton is the button under the list of clothes in the registration card.
+This eventhandler takes care of the validation of the inputfields according to the kind of transaction chosen.
+If "Handover" is chosen, it calls the functions "checkIfArrayIsEmpty" and "checkIfInputIsEmpty" which will return true, if inputs were made.
+Then it will build the successcard.
+If "Pickup" is chosen, the eventhandler will call the function "checkInputs".
+There should be now failure until now, but if in some case the kind of transaction isn't one of these two options, there will be an alert to restart the page.
+*/ 
 submitBtn.onclick = function () {
     
     if(kindOfTransaction === "Übergabe") {
@@ -126,11 +153,13 @@ submitBtn.onclick = function () {
                         
 
     } else {
-        //console.log("Fehler") //TODO Errorhandling
+        alert("Es ist ein Fehler aufgetreten. Bitte lade die Seite erneut!")
     }
 
 }
 
+
+//This eventhandler reloads the registrationform
 btnNewDonation.onclick = function () {
     inputFirstName.value = ""
     inputLastName.value = ""
@@ -147,6 +176,7 @@ btnNewDonation.onclick = function () {
 }
 
 
+//This function shows the startcard and hides the others
 function showStart () {
     cardStart.style.display = "block"
     cardRegistrationForm.style.display = "none"
@@ -156,6 +186,7 @@ function showStart () {
 
 }
 
+//This function shows the errorcard and hides the others
 function showError() {
     cardStart.style.display = "none"
     cardRegistrationForm.style.display = "none"
@@ -165,6 +196,7 @@ function showError() {
 
 }
 
+//This function shows the successcard and hides the others
 function showSuccess() {
     cardSuccess.style.display = "block"
     cardRegistrationForm.style.display = "none"
@@ -172,6 +204,7 @@ function showSuccess() {
 
 }
 
+//This function shows the registrationform and hides the other cards
 function showRegistrationForm() {
     cardStart.style.display = "none"
     cardRegistrationForm.style.display = "block"
@@ -181,20 +214,14 @@ function showRegistrationForm() {
 }
 
 
-
-function checkZIPCode() {
-    const digits = inputZIPCode.value.toString().slice(0,2)
-    if (digits === "93") {
-        buildSuccessCard();
-    } else {
-        alert("Es tut uns leid! Leider ist Deine Abholungsadresse zu weit weg von unserer Regensburger Filiale!")
-        //TODO Fehlerhandling
-    }
-
-    
-    
-}
-
+/*
+This function gets called earlier in the script to make sure, that inputs are filled properly.
+It introduces an array of the inputs and iterates over them, calling another function called "checkIfInputIsFilled" which will return "true" if the input isn't empty. 
+In that case it increments the counter by 1.
+Then it makes sure, that the zipcode-input contains exactly 5 digits.
+If everything is fine, the counter will be "6".
+If thats the case and the list of clothes isn't empty, "checkInputs" will call the function to check the zipcode to compare the first two digits.
+*/
 function checkInputs () {
     const inputs = [inputFirstName, inputLastName, inputStreet, inputZIPCode, inputCity, selectConflictArea]
     let counter = 0
@@ -222,6 +249,28 @@ function checkInputs () {
     }
 }
 
+/*
+This compares the first two digits of the zipcode to the String "93".
+*/
+function checkZIPCode() {
+    const digits = inputZIPCode.value.toString().slice(0,2)
+    if (digits === "93") {
+        buildSuccessCard();
+    } else {
+        alert("Es tut uns leid! Leider ist Deine Abholungsadresse zu weit weg von unserer Regensburger Filiale!")
+        //TODO Fehlerhandling
+    }
+
+    
+    
+}
+
+
+/*
+This function gets an array and makes sure, its not empty. In this case, its the array that contains the list of clothes.
+If there is at least one item in the list, the inputform for the clothing will be bordered grey and the function returns "true".
+If the array is empty, the input will be bordered red and the function returns false.
+*/
 function checkIfArrayIsEmpty (arr) {
     
     if (arr.length > 0) {
@@ -232,32 +281,44 @@ function checkIfArrayIsEmpty (arr) {
         return false; }
 }
 
+/*
+This function is called ealier with an array of inputs.
+It will assure, that the given inputs aren't empty. It'll return "false" in that case and make the given input bordered red.
+If everything is fine, it'll return "true" and changes the bordercolor to grey.
+*/
 function checkIfInputIsFilled (input) {
     if (input.value != "") {
-        console.log("Erfolg in InputIsFilled");
         input.style["border-color"] = "lightgrey";
         return true; 
     } else { 
-        console.log("Fehler in InputIsFilled");
         input.style["border-color"] = "red";
         return false; 
     }
 }
 
+
+/**
+This function will build the successcard if every check wen't fine until now.
+At first it introduces the constant "date" for a timestamp.
+Then it will build the timeStampDate and the timeStampTime.
+
+Then it will build the body of the successcard, using the timestamp and the listOfClothes to return the data to the customer.
+Finally it will call the function "showSuccess" to show the card and hide the others.
+*/
 function buildSuccessCard () {
 
+    //Building the timestamp
     const date = new Date();
     const timeStampDate = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear()
     const timeStampTime = () => {
         let hours 
         let minutes
-        if(date.getHours() < 10) {
+        if(date.getHours() < 10) { //add a zero, if it only has one digit
             hours = "0" + date.getHours()
         } else { hours = date.getHours()}
 
         
-        if(date.getMinutes() < 10) {
-
+        if(date.getMinutes() < 10) { //add a zero, if it only has one digit
             minutes = "0" + date.getMinutes()
         } else { minutes = date.getMinutes()}
 
@@ -266,6 +327,7 @@ function buildSuccessCard () {
     }
 
     
+    //Building the card
     const successCardBody = document.getElementById("successCardBody")
 
     let k = ""
@@ -286,6 +348,8 @@ function buildSuccessCard () {
     showSuccess();
 }
 
+
+//This function gets called directly inside the HTML to show a Lorem Ipsum Page in case one of the links in the footer gets called.
 function showLoremIpsum() {
     cardStart.style.display = "none"
     cardError.style.display = "none"
